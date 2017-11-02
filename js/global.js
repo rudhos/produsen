d6dd91cd6ae6d2540e5f37a93a4c31d2c46bdb37
Rudhi Sasmito <rudhisasmito@gmail.com>
2017-11-02 22:01:37 +0700

    Order + sidemenu + modal

--- js/global.js
+++ js/global.js
@@ -3,17 +3,22 @@ $(document).ready(function() {
 	/*-----------------------------------/
 	/*	TOP NAVIGATION AND LAYOUT
 	/*----------------------------------*/
+	var $body = $('body');
+	var $overlay = $('.overlay');
 
 	$('.btn-burger').on('click', function() {
-		if(!$('body').hasClass('sidebar-show')) {
-			$('body').addClass('sidebar-show');
-
+		if(!$body.hasClass('sidebar-show')) {
+			$body.addClass('sidebar-show');
+			$overlay.fadeIn();
 		} else {
-			$('body').removeClass('sidebar-show');
-			$('body').removeClass('layout-default'); // also remove default behaviour if set
+			$body.removeClass('sidebar-show');
+			$body.removeClass('layout-default'); 
+			$overlay.fadeOut();
 		}
-
 	});
-
+	$overlay.on('click', function() {
+		$(this).fadeOut();
+		$body.removeClass('sidebar-show');
+	});
 
 });
